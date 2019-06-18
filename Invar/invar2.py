d406836e99d5eab1f18bfea7d831c64cd813aa0b
KsushaSeliv <KsenijaSeliv@yandex.ru>
2019-06-18 23:59:05 +0300

    Create invar2.py

---
+++ Invar/invar2.py
@@ -0,0 +1,21 @@
+#Разработать фрагмент программы с использованием библиотеки pyqrcode, позволяющей создавать изображение QR-кода на основе переданной в
+#программу текстовой строки.
+#Реализовать модификацию изображения генерируемого QR-кода: раскрасить фрагменты изображения в несколько случайно определяемых цветов.
+
+import pyqrcode
+import png
+
+def Qr(content, module_color, background, file_format, scale):
+#content-нужная нам строка
+#module_color-wdtn qr кода
+#background-фон
+#file_format-формат сохраняемого qr-кода
+#scale-масштаб 
+    qrcode = pyqrcode.create(content)
+    if file_format == 'png':
+        qrcode.png('qr.png', module_color = module_color, background=background,scale=scale)
+    elif file_format == 'svg':
+        qrcode.svg('qr.svg', module_color = module_color, background=background,scale=scale)
+
+result = input('Текстовая строка: ')
+Qr(result, (0,50,99), (2,5,6), file_format = 'png', scale = 8)
